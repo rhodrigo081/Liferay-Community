8c56b728040fff145a56e31ea11de1253e22ebd9
Victor-Nero <149793782+Victor-Nero@users.noreply.github.com>
2025-05-15 20:22:51 -0300

    Terminei as comunidades

--- liferay-Community/src/components/CommunityView/CommunityView.tsx
+++ liferay-Community/src/components/CommunityView/CommunityView.tsx
@@ -1,73 +1,113 @@
 import {
-    CardsTitle,
-    CardsContainer,
-    Card,
-    CardButton,
-    CardTitle,
-    CardDescription,
-    WorkBadge,
-  } from "./styles";
-  
-  export function CommunityView() {
-    return (
-    
+  CardsTitle,
+  CardsContainer,
+  Card,
+  CardButton,
+  CardTitle,
+  CardDescription,
+  WorkBadge,
+  CardBanner,
+  CardContent
+} from "./styles";
+
+export function CommunityView() {
+  return (
+    <CardsContainer>
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerJava.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>Java Developers</CardTitle>
+          <CardDescription>Este grupo é dedicado a estudantes e profissionais que querem aprender e evoluir no desenvolvimento com Java. </CardDescription>
+         <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
       
-          <CardsContainer>
-            <Card>
-              <img src="" alt="" />
-              <CardTitle>Front-end Brasil</CardTitle>
-              <CardDescription>React, Vue, Angular e mais.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Back-end Devs</CardTitle>
-              <WorkBadge>Trabalho</WorkBadge> 
-              <CardDescription>Node, Java, Python, bancos de dados.</CardDescription>
-              <div style={{ display: 'flex', gap: '1rem', marginBottom: '2rem' }}>
-            
-            
-          </div>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-            
-            <Card>
-              <CardTitle>Mobile Lovers</CardTitle>
-              <CardDescription>React Native, Flutter, iOS e Android.</CardDescription>
-              <CardButton>Entrar</CardButton>
-            </Card>
-          </CardsContainer>
-    
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerING.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>English Studants</CardTitle>
+          <CardDescription>Se você quer melhorar seu inglês, este é o lugar certo. Aqui praticamos conversação, trocamos dicas de gramática, vocabulário e expressões cotidianas.</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
       
-    );
-  }
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerVolei.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>Jogadores de Vôlei </CardTitle>
+          <CardDescription>Para quem ama vôlei e quer trocar experiências sobre o esporte. Aqui você pode discutir técnicas, estratégias de jogo, acompanhar campeonatos.</CardDescription>
+          <WorkBadge>Esporte</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+      
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerCS.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>CS:GO.BR</CardTitle>
+          <CardDescription>Se Counter-Strike é o seu jogo favorito, este grupo foi feito para você. Monte times, jogue partidas competitivas, troque dicas sobre...</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+      
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerBoleiro.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>Boleiros</CardTitle>
+          <CardDescription>Se futebol é a sua paixão, este grupo é para você. Aqui falamos sobre partidas, lances memoráveis, organizamos jogos entre os...</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+      
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerLeitores.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>Leitores de Plantão</CardTitle>
+          <CardDescription>Um espaço para quem ama livros e quer trocar experiências sobre literatura. Compartilhe indicações de livros, participe de discussões.</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+      
+      <Card>
+       <CardBanner>
+          <img src="/src/assets/BannerFIFA.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>FIFA Club</CardTitle>
+          <CardDescription>Para quem é fã de FIFA e quer se conectar com outros jogadores. Organize partidas, participe de ligas e campeonatos, compartilhe estratégias e.</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+      
+      <Card>
+        <CardBanner>
+          <img src="/src/assets/BannerPet.jpg" />
+        </CardBanner>
+        <CardContent>
+          <CardTitle>Pais de Pet</CardTitle>
+          <CardDescription>Se você considera seu pet parte da família, este grupo é para você. Aqui você pode compartilhar fotos, trocar experiências, tirar dúvidas sobre...</CardDescription>
+          <WorkBadge>Trabalho</WorkBadge>
+          <CardButton>Acessar</CardButton>
+        </CardContent>
+      </Card>
+    </CardsContainer>
+  );
+}
--- liferay-Community/src/components/CommunityView/styles.ts
+++ liferay-Community/src/components/CommunityView/styles.ts
@@ -1,16 +1,12 @@
 import styled from "styled-components";
 
-
-
-
 export const CardsTitle = styled.h3`
-  
   margin-bottom: 1.5rem;
 `;
 
 export const CardsContainer = styled.div`
-display: flex;  
-flex-wrap: wrap;
+  display: flex;  
+  flex-wrap: wrap;
   gap: 1.5rem;
   margin-bottom: 1.5rem;
 `;
@@ -18,30 +14,56 @@ flex-wrap: wrap;
 export const Card = styled.div`
   background-color: ${(props) => props.theme.card?.background || '#00072B'};
   border-radius: 5px;
-  padding: 1rem;
   transition: transform 0.2s ease;
+  width: 20rem;
+  height: 20rem;
+  position: relative; /* Adicionado para permitir posicionamento absoluto interno */
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
 
   &:hover {
     transform: translateY(-4px);
   }
+`;
+
+export const CardBanner = styled.div`
+  width: 100%;
+  height: 6rem;
+  position: relative;
+  overflow: hidden;
   
   img {
     width: 100%;
-    border-radius: 4px;
+    height: 100%;
+    object-fit: cover;
   }
 `;
 
+
+
+
+export const CardContent = styled.div`
+  padding: 1rem;
+ 
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
 export const CardTitle = styled.h3`
+
   margin-bottom: 0.5rem;
   color: ${(props) => props.theme.text || 'white'};
 `;
 
 export const CardDescription = styled.p`
   color: ${(props) => props.theme.lightText || '#bababa'};
+  font-size: 0.8rem;
+  margin-bottom: 1rem;
 `;
 
 export const CardButton = styled.button`
-  margin-top: 0.5rem;
   padding: 0.5rem 1rem;
   background-color: ${(props) => props.theme.button?.background || '#2563eb'};
   color: ${(props) => props.theme.button?.text || 'white'};
@@ -49,6 +71,9 @@ export const CardButton = styled.button`
   border-radius: 8px;
   cursor: pointer;
   transition: background-color 0.2s;
+  position: absolute;
+  bottom: 1rem;
+  left: 1rem;
 
   &:hover {
     background-color: ${(props) => props.theme.button?.hover || '#1d4ed8'};
@@ -56,22 +81,19 @@ export const CardButton = styled.button`
 `;
 
 export const WorkBadge = styled.div`
-  margin-top: 0.5rem;
-  padding: 0.5rem 1rem;
-   background-color: ${(props) => props.theme.button?.background || '#272833'};
-  color: ${(props) => props.theme.button?.text || 'white'};
-  border-radius: 10px;
+  display: inline-block;
+  padding: 0.25rem 0.75rem;
+  background-color: ${(props) => props.theme.button?.background || '#272833'};
   color: #DCDDE4;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  border-radius: 10px;
   font-weight: bold;
-  width: 80px;
-  height: 20px;
   font-size: 14px;
+  margin-bottom: 0.75rem;
+  width: fit-content;
+  
   &:hover {
     background-color: ${(props) => props.theme.button?.hover || '#1d4ed8'};
-
+  }
 `;
 
 export const WorkBadgeWithIcon = styled.div`
